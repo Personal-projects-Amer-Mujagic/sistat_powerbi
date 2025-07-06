d2c4183ec9f564e48c5301e867a3c063507fb62e
Amer Mujagić <amer.mujagic.am@gmail.com>
2025-07-06 17:35:20 +0200

    added logging and error handling, added bruto/neto column

--- data_preparation.py
+++ data_preparation.py
@@ -37,14 +37,30 @@ except requests.exceptions.RequestException as e:
     logging.error(f"Request failed: {e}")
     sys.exit(1)
 
+# The response is a CSV file
+# Split table into rows
 rows = response.text.split("\r\n")
+# Split rows into cells
 rows = [row.split(",") for row in rows if row.strip()]
+# Remove extra characters
 header = [cell.strip('"') for cell in rows[0]]
 df = pd.DataFrame(rows[1:], columns=header)
 
+# Remove extra characters
 df["SEKTOR"] = df["SEKTOR"].str.strip('"')
 df["MESEC"] = df["MESEC"].str.strip('"')
+
+# Split MESEC into LETO and MESEC
 df[["LETO", "MESEC"]] = df["MESEC"].str.split("M", expand=True)
+
+# Create a Bruto and Neto category
+df_bruto = df.iloc[:,[0,1,4]].copy()
+df_neto = df.iloc[:,[0,1,4]].copy()
+df_bruto["Bruto/Neto"] = "Bruto"
+df_bruto["Plača za mesec (EUR)"] =  df.iloc[:, 2]
+df_neto["Bruto/Neto"] = "Neto"
+df_neto["Plača za mesec (EUR)"] =  df.iloc[:, 3]
+df = pd.concat([df_bruto, df_neto])
 
 # Write to Excel
 try:
